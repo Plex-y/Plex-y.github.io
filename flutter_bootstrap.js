3b87a86c25d7a5936bbc7ea40ea78b81b11e0e0f
Plex-y <maxi.jmm@gmail.com>
2025-07-07 17:41:05 -0300

    Deploy automÃ¡tico del sitio Flutter

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4231864541"
+    serviceWorkerVersion: "2303114758"
   }
 });
